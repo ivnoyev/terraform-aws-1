91439ebc30b27b22c46111eec403efd66517507e
Ivan Noev <Ivan.Noev@madnesspartners.com>
2025-10-21 15:11:27 +0500

    fixed instance type

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "learn-terraform"
